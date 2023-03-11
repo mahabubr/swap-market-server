6939610f931fefac9104268e807ee47610002e96
mahabubr <mahabub.r753@gmail.com>
2023-03-11 23:52:12 +0600

    jwt token implement

--- index.js
+++ index.js
@@ -4,16 +4,32 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 // middle were
 app.use(cors())
 app.use(express.json())
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_KEY, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 app.get('/', (req, res) => {
     res.send("Swap Market Server is Running")
 })
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vlhy1ml.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -22,13 +38,26 @@ async function run() {
     try {
         const productCollection = client.db('SWAP-MARKET').collection('products')
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_KEY, { expiresIn: '1d' })
+            res.send({ token })
+        })
+
         app.post('/product', async (req, res) => {
             const product = req.body
             const result = await productCollection.insertOne(product)
             res.send(result)
         })
 
-        app.get('/product', async (req, res) => {
+        app.get('/product', verifyJWT, async (req, res) => {
+
+            const decoded = req.decoded;
+
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'Unauthorized access' })
+            }
+
             const page = parseInt(req.query.page);
             const size = parseInt(req.query.size);
             let query = {}
@@ -42,7 +71,7 @@ async function run() {
             res.send({ count, products });
         })
 
-        app.get('/product/:id', async (req, res) => {
+        app.get('/product/:id', verifyJWT, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const service = await productCollection.findOne(query)
