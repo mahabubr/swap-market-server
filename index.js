d520957d66abe108cdeea55198acb9d21140cadc
mahabubr <mahabub.r753@gmail.com>
2023-03-11 22:24:07 +0600

    pagination done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middle were
 app.use(cors())
@@ -20,9 +20,57 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        const swapMarketCollection = client.db('SWAP-MARKET').collection('products')
+        const productCollection = client.db('SWAP-MARKET').collection('products')
 
-        
+        app.post('/product', async (req, res) => {
+            const product = req.body
+            const result = await productCollection.insertOne(product)
+            res.send(result)
+        })
+
+        app.get('/product', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            let query = {}
+            // if (req.query.email) {
+            //     query = {
+            //         email: req.query.email
+            //     }
+            // }
+            const products = await productCollection.find(query).skip(page * size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count, products });
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const service = await productCollection.findOne(query)
+            res.send(service)
+        })
+
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const data = req.body
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    img: data.img,
+                    name: data.name,
+                    price: data.price
+                }
+            }
+            const result = await productCollection.updateOne(filter, updatedDoc, options)
+            res.send(result)
+        })
+
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await productCollection.deleteOne(query)
+            res.send(result)
+        })
 
     } catch (error) {
         console.log(error);
